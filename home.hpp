6f95213b040f1f07f427e5681619f4189b7dd3bf
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2025-03-09 23:55:21 -0700

    hell yeah

--- home.hpp
+++ home.hpp
@@ -9,35 +9,35 @@ constexpr auto home() {
     using namespace html;
     return layout(
             h3<"text-2xl font-bold text-center mt-4">{a{"jamie@pond.audio"}.with_href(links::EMAIL)},
-            p {{
+            p {
                "I'm Lead Audio Software Engineer at ", a{"mayk"}
                .with_href(links::MAYK)
-            }},
-            p {{
+            },
+            p {
               "I studied ", span<"font-bold">{"Sound & Music Computing MSc"},
               " at ", a<"text-blue-500"> {"Queen Mary University of London"}
               .with_href(links::QMUL)
-            }},
-            p {{
+            },
+            p {
               "I spoke at ADC 2021 about ", a{span<"font-bold text-blue-500">{"using compiler intrinsics in your code"}}
               .with_href(links::ADC_2021)
-            }},
+            },
             a <"text-blue-500"> {
               "Click here to go to emily's page"
             }.with_href("/emily"),
-            p {{
+            p {
               "I spoke at ADC 2023 about ", a{span<"font-bold">{"mayk's approach to prototyping"}}
               .with_href(links::ADC_2023)
-            }},
+            },
             p {"I'm a mentor in the ADC 2023 & 2024 Mentorship Program."},
-            p {{
+            p {
               a<"underline">{"Please feel free to hit me up!"}
               .with_href(links::EMAIL)
-            }},
-            html::div<"flex flex-row space-x-4">{{
+            },
+            html::div<"flex flex-row space-x-4">{
                 a{img<"w-8 h-8">{}.with_src(links::LINKEDIN_IMG)}.with_href(links::LINKEDIN),
                 a{img<"w-8 h-8">{}.with_src(links::TWITTER_IMG)}.with_href(links::TWITTER)
-            }}
+            }
     );
 }
 
