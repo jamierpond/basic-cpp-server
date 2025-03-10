2f0c2a66fc9b313909d1237b00714eca2ea81bec
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2025-03-10 01:21:24 -0700

    fmt

--- home.hpp
+++ home.hpp
@@ -5,17 +5,19 @@
 static_assert(html::p <"text-blue-500">{"p"}.render() == "<p class='text-blue-500'>p</p>");
 
 
+static_assert(html::p{"foo"}.render() == "<p>foo</p>");
+
 constexpr auto home() {
     using namespace html;
     return layout(
             h3<"text-2xl font-bold text-center mt-4">{a{"jamie@pond.audio"}.with_href(links::EMAIL)},
             p {
-               "I'm Lead Audio Software Engineer at ", a{"mayk"}
-               .with_href(links::MAYK)
+               "I'm Lead Audio Software Engineer at ",
+               a{"mayk"}.with_href(links::MAYK)
             },
             p {
               "I studied ", span<"font-bold">{"Sound & Music Computing MSc"},
-              " at ", a<"text-blue-500"> {"Queen Mary University of London"}
+              " at ", a<"text-blue-500">{"Queen Mary University of London"}
               .with_href(links::QMUL)
             },
             p {
@@ -31,7 +33,7 @@ constexpr auto home() {
             },
             p {"I'm a mentor in the ADC 2023 & 2024 Mentorship Program."},
             p {
-              a<"underline">{"Please feel free to hit me up!"}
+              a<"text-blue-500 underline">{"Please feel free to hit me up!"}
               .with_href(links::EMAIL)
             },
             html::div<"flex flex-row space-x-4">{
