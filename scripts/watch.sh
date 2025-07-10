b9ead7144798be46a2e8aa9f2b5524a9aa4763a5
Jamie Pond <jamiepond259@gmail.com>
2025-07-10 01:50:57 -0700

    wip

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,14 +2,8 @@
 
 cd /app
 
-# Watch for changes and rebuild using Ninja
-find ./src -name '*.cpp' -o -name '*.h' | \
+find ./src ./test -name '*.cpp' -o -name '*.hpp' -o -name '*.h' | \
   entr -r sh -c '
-    cmake -S . -B build \
-      -G Ninja \
-      -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_C_COMPILER=clang \
-      -DCMAKE_CXX_COMPILER=clang++ && \
-    cmake --build build && \
-    ./build/PondAudio'
+    cmake --build build/debug && \
+    ./build/debug/PondAudio'
 
